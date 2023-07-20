554ab97435e16def6f8243762e19501c156bf136
PedroPB9 <pedropasqualb@gmail.com>
2023-07-20 08:56:56 -0300

    testing short and int relationship

--- type-variables/src/TypeVariables.java
+++ type-variables/src/TypeVariables.java
@@ -2,4 +2,9 @@ public class TypeVariables {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    short length = 100;
+    int length2 = length;
+
+    short width = (short)length2;
 }
