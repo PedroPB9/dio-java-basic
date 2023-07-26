1d9ebacbafb9abcb82c2b145a591127199bbd04e
PedroPB9 <pedropasqualb@gmail.com>
2023-07-26 08:20:20 -0300

    verifying possibilities of concatenating strings

--- type-variables/src/TypeVariables.java
+++ type-variables/src/TypeVariables.java
@@ -1,10 +1,17 @@
 public class TypeVariables {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        short length = 100;
+        int length2 = length;
+
+        short width = (short)length2;
+
+        String one = " ";
+
+        one = "1" + (1+1);
+        System.out.println(one);
     }
 
-    short length = 100;
-    int length2 = length;
+    
 
-    short width = (short)length2;
 }
