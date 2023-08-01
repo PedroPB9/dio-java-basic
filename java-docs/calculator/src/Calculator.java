bd471878fc87ce3e5958ad290b9469bb7c094a41
PedroPB9 <pedropasqualb@gmail.com>
2023-08-01 10:35:18 -0300

    creating a documentated class

---
+++ java-docs/calculator/src/Calculator.java
@@ -0,0 +1,20 @@
+/**
+ * <h1> Calculator </h1>
+ * This class sums two values
+ * <p>
+ * <b>Note:</b> Read the documentation carefully
+ * @author Pedro Pasqual
+ * @version 0.1
+ * @since 1/8/23
+ */
+public class Calculator {
+    /**
+     * This method takes two parameters and sum them
+     * @param firstNumber
+     * @param secondNumber
+     * @return an integer is returned, which represents the sum between the parameters
+     */
+    public int sum(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+}
